2d76213b9a79e23762e35d7dbf94f4f6738b7132
martinarrose <mmartinarrose@gmail.com>
2026-04-24 12:06:31 +0200

    init 2.0: restart the entire project

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,3 +1,24 @@
 export default function AppFooter() {
-  return <footer>footeer</footer>;
+  return (
+    <footer>
+      <div className="footer1">
+        <div>DIGITAL COMICS</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="footer2">
+        <div>CD COMICS</div>
+        <div></div>
+      </div>
+      <div></div>
+      <div></div>
+
+      <div className="footer3">
+        <button className="button">SIGN-UP NOW!</button>
+      </div>
+    </footer>
+  );
 }
